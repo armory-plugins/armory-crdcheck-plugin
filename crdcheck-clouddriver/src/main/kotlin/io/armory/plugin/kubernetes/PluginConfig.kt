ff1f5fa8a16421613183829c9995628f0b2af3cc
edgarulg <edgar.garcia@armory.io>
2020-06-26 17:26:07 -0500

    fix(status): add dash to property

--- crdcheck-clouddriver/src/main/kotlin/io/armory/plugin/kubernetes/PluginConfig.kt
+++ crdcheck-clouddriver/src/main/kotlin/io/armory/plugin/kubernetes/PluginConfig.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-@ConfigurationProperties("armory.crdCheck")
+@ConfigurationProperties("armory.crd-check")
 data class PluginConfig (
         val kind: String,
         val apiGroup: String
