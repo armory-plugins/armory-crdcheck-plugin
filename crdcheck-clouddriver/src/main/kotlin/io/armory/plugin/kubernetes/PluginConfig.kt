4723ea95b1fa9a408adaeba622b81bfcb8039df9
edgarulg <edgar.garcia@armory.io>
2020-06-26 16:39:20 -0500

    feat(status): rename configs

--- crdcheck-clouddriver/src/main/kotlin/io/armory/plugin/kubernetes/PluginConfig.kt
+++ crdcheck-clouddriver/src/main/kotlin/io/armory/plugin/kubernetes/PluginConfig.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-@ConfigurationProperties("spinnaker.extensibility.plugins.armory.CRDCheck.config")
+@ConfigurationProperties("armory.crdCheck")
 data class PluginConfig (
         val kind: String,
         val apiGroup: String
