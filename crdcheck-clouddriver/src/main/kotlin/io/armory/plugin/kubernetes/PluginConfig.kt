1a043a34a879be76aac1fe9e495cd04958a6ffa8
edgarulg <edgar.garcia@armory.io>
2020-06-26 17:55:34 -0500

    fix(status): change to variables

--- crdcheck-clouddriver/src/main/kotlin/io/armory/plugin/kubernetes/PluginConfig.kt
+++ crdcheck-clouddriver/src/main/kotlin/io/armory/plugin/kubernetes/PluginConfig.kt
@@ -1,11 +1,9 @@
 package io.armory.plugin.kubernetes
 
 import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.context.annotation.Configuration
 
-@Configuration
 @ConfigurationProperties("armory.crd-check")
 data class PluginConfig (
-        val kind: String,
-        val apiGroup: String
+        var kind: String? = null,
+        var apiGroup: String? = null
 )
