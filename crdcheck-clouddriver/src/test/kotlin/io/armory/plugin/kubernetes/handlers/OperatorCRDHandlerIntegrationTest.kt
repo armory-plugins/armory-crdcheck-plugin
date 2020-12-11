73ff400850eaff73d6bf44019a47ca30ce1b759f
Dan Peach <daniel.peach@armory.io>
2020-12-11 15:41:28 -0500

    fix test

--- crdcheck-clouddriver/src/test/kotlin/io/armory/plugin/kubernetes/handlers/OperatorCRDHandlerIntegrationTest.kt
+++ crdcheck-clouddriver/src/test/kotlin/io/armory/plugin/kubernetes/handlers/OperatorCRDHandlerIntegrationTest.kt
@@ -1,15 +1,17 @@
 package io.armory.plugin.kubernetes.handlers
 
-import com.netflix.spinnaker.kork.plugins.tck.serviceFixture
 import dev.minutest.junit.JUnit5Minutests
 import dev.minutest.rootContext
+import org.springframework.test.context.TestContextManager
 import strikt.api.expectThat
 import strikt.assertions.isNotNull
 
 class OperatorCRDHandlerIntegrationTest : JUnit5Minutests {
     fun tests() = rootContext<OperatorCRDHandlerTestFixture> {
-        serviceFixture {
-            OperatorCRDHandlerTestFixture()
+        fixture {
+            OperatorCRDHandlerTestFixture().also {
+                TestContextManager(OperatorCRDHandlerTestFixture::class.java).prepareTestInstance(it)
+            }
         }
 
         test("can fetch handler from application context") {
