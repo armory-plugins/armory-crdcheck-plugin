31226ea6d9dff0f35654f2f6a4882944b1e05478
edgarulg <edgar.garcia@armory.io>
2021-07-08 09:16:32 -0500

    chore(dependencies): upgrade minutest

--- crdcheck-clouddriver/src/test/kotlin/io/armory/plugin/kubernetes/handlers/OperatorCRDHandlerIntegrationTest.kt
+++ crdcheck-clouddriver/src/test/kotlin/io/armory/plugin/kubernetes/handlers/OperatorCRDHandlerIntegrationTest.kt
@@ -2,6 +2,7 @@ package io.armory.plugin.kubernetes.handlers
 
 import dev.minutest.junit.JUnit5Minutests
 import dev.minutest.rootContext
+import dev.minutest.test
 import org.springframework.test.context.TestContextManager
 import strikt.api.expectThat
 import strikt.assertions.isNotNull
